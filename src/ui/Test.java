f614b0eaee880a2f929c534db7be058eed302336
guangmingzhengda <1345604128@qq.com>
2023-12-03 11:16:28 +0800

    默认更改列表

--- src/ui/Test.java
+++ src/ui/Test.java
@@ -1,10 +1,22 @@
 package ui;
 
+import Click.ProvinceDetail;
+import Game.Hamiltonian;
+import Game.HamiltonianDetial;
 import oop.zsz.client.AppClient;
+
+import java.util.Arrays;
+import java.util.Random;
 
 public class Test {
 
     public static void main(String[] args) throws InterruptedException {
-        
+
+//        Hamiltonian.pre();
+//        String[] s={"beijing","henan","shanxi","hainan","shaanxi","hebei","guangdong","taiwan","xianggang","aomen"};
+//        HamiltonianDetial a=Hamiltonian.calcTime(s);
+//        System.out.println(Arrays.toString(a.path));
+//        System.out.println(a.cost);
+//        System.out.println(a.found);
     }
 }
